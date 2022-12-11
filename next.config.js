ebdbc7cf66e203147f00b21f1d7553c2242eae63
aditiyakshk499 <adiaditiyakshk499@gmail.com>
2022-12-11 14:42:03 +0530

    added eslinting rule set

--- next.config.js
+++ next.config.js
@@ -5,5 +5,9 @@ const nextConfig = {
 }
 
 module.exports = {
- 
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
